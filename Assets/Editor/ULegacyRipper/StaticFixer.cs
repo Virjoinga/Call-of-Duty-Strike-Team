45696e2311eee379c4d345dbb07bbcad1cb6c16d
Virjoinga <ethanbest51@gmail.com>
2025-08-13 13:48:22 +1200

    add new version of shader translator

---
+++ Assets/Editor/ULegacyRipper/StaticFixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public class StaticFixer : Editor
+{
+	[MenuItem("Static Fixes/Fix Static Objects")]
+	public static void Fix()
+	{
+		foreach (GameObject gameObject in FindObjectsOfType<GameObject>())
+		{
+			MeshRenderer renderer = gameObject.GetComponent<MeshRenderer>();
+
+			if (renderer != null)
+			{
+				gameObject.isStatic = renderer.isPartOfStaticBatch;
+				continue;
+			}
+
+			gameObject.isStatic = false;
+		}
+
+		EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+	}
+}
